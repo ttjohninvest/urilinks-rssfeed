740d58ffc53c8fb6bca52f73f80b61f9ae04e6b8
ttjohninvest <ttjohninvest@gmail.com>
2026-02-05 15:23:37 -0800

    2

--- server.js
+++ server.js
@@ -30,28 +30,27 @@ app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => {
-  //async
-  function convertRSSToJSON(rssURL) {
+  async function convertRSSToJSON(rssURL) {
   const apiURL = "https://api.apyhub.com/convert/rss-url/json?detailed=true";
-  // const requestData = { url: rssURL };
-  // const options = {
-  //   method: "POST",
-  //   url: apiURL,
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     "apy-token": "APY0hVYCmwgWbHhNIpvFZ2hjHQIYEnWJx32MrM3Cn40u4y0Upq9NGH2FPKFP3kP5GQdyYrgbPlKWqf" // Replace with your actual token
-  //   },
-  //   data: requestData
-  // };
+  const requestData = { url: rssURL };
+  const options = {
+    method: "POST",
+    url: apiURL,
+    headers: {
+      "Content-Type": "application/json",
+      "apy-token": "APY0hVYCmwgWbHhNIpvFZ2hjHQIYEnWJx32MrM3Cn40u4y0Upq9NGH2FPKFP3kP5GQdyYrgbPlKWqf" // Replace with your actual token
+    },
+    data: requestData
+  };
 
-  // try {
-  //   const response = await axios.request(options);
-  //   console.log("RSS to JSON conversion successful:", response.data);
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("An error occurred:", error.message);
-  //   throw error;
-  // }
+  try {
+    const response = await axios.request(options);
+    console.log("RSS to JSON conversion successful:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+    throw error;
+  }
 }
 
 convertRSSToJSON("https://www.espn.com/espn/rss/nfl/news")
